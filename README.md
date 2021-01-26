637166ddab3e6c42a6d279e8e380a7c738fde8eb
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-01-26 18:38:31 +1300

    feat: init project

---
+++ README.md
@@ -0,0 +1,3 @@
+**AutoCode** 
+
+## Heading
---
+++ src/AutoCode.cs
@@ -0,0 +1,12 @@
+using Oxide.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Oxide.Plugins
+{
+    [Info("Auto Code", "slaymaster3000", "0.0.0")]
+    class AutoCode : RustPlugin
+    {
+        
+    }
+}
