cf0d6c1f410ddaa05828eebe8cc06e89c0d60ea1
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-02-14 02:36:20 +1300

    build: have release version of plugin at root
    this allows for better umod intergation with it's current implementation

--- scripts/prepareForRelease.js
+++ scripts/prepareForRelease.js
@@ -2,7 +2,8 @@ const { promises: fs } = require("fs");
 const path = require("path");
 const semver = require("semver");
 
-const filePath = path.join(process.cwd(), "src/AutoCode.cs");
+const srcPath = path.join(process.cwd(), "src/AutoCode.cs");
+const distPath = path.join(process.cwd(), "AutoCode.cs");
 
 const args = process.argv.slice(2);
 const newVersion = semver.valid(args[0]);
@@ -11,11 +12,15 @@ if (newVersion === null) {
   process.exit(1);
 }
 
+const header = '// This is an auto generated file. Please edit "src/AutoCode.cs" instead.';
+
 (async () => {
-  const fileContent = await fs.readFile(filePath, { encoding: "utf-8" });
-  const updatedFileContent = fileContent.replace(
-    "0.0.0-development",
-    newVersion
-  );
-  await fs.writeFile(filePath, updatedFileContent, { encoding: "utf-8" });
+  const fileContent = await fs.readFile(srcPath, { encoding: "utf-8" });
+  const updatedFileContent = `${header}\n\n${
+    fileContent
+      .replace("#define DEBUG", "")
+      .replace("0.0.0-development", newVersion)
+      .trimStart()
+    }`;
+  await fs.writeFile(distPath, updatedFileContent, { encoding: "utf-8" });
 })();
