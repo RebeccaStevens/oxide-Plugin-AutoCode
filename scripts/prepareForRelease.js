b2b86fd6d1a95bbc16bbd63426f2c74e4418e029
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2021-04-20 17:56:21 +1200

    build: adjust build config and process

--- scripts/prepareForRelease.js
+++ scripts/prepareForRelease.js
@@ -2,7 +2,8 @@ const { promises: fs } = require("fs");
 const path = require("path");
 const semver = require("semver");
 
-const srcPath = path.join(process.cwd(), "src/AutoCode.cs");
+const src = "src/AutoCode.cs";
+const srcPath = path.join(process.cwd(), src);
 const distPath = path.join(process.cwd(), "AutoCode.cs");
 
 const args = process.argv.slice(2);
@@ -12,15 +13,12 @@ if (newVersion === null) {
   process.exit(1);
 }
 
-const header = '// This is an auto generated file. Please edit "src/AutoCode.cs" instead.';
+const header = `// This is an auto generated file. Please edit "${src}" instead.`;
 
 (async () => {
   const fileContent = await fs.readFile(srcPath, { encoding: "utf-8" });
-  const updatedFileContent = `${header}\n\n${
-    fileContent
-      .replace("#define DEBUG", "")
-      .replace("0.0.0-development", newVersion)
-      .trimStart()
-    }`;
+  const updatedFileContent = `${header}\n\n${fileContent
+    .replace("0.0.0-development", newVersion)
+    .trimStart()}`;
   await fs.writeFile(distPath, updatedFileContent, { encoding: "utf-8" });
 })();
